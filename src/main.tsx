7f5d2eca925200daf409be78511f9ad3c737a677
senid-221 <iraguhavincent11@gmail.com>
2026-09-03 21:28:56 +0200

    Use server cookie authentication without Clerk provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { ClerkProvider } from '@clerk/clerk-react';
 import App from './App.tsx';
 import './index.css';
 
-const clerkPublishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-if (!clerkPublishableKey) {
-  throw new Error('VITE_CLERK_PUBLISHABLE_KEY is required. Configure it in the production environment.');
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ClerkProvider publishableKey={clerkPublishableKey}>
-      <App />
-    </ClerkProvider>
+    <App />
   </StrictMode>,
 );
